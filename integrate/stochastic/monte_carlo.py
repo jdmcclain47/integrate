d162f7ff94c5284bcba4a18dea0f0c4c4949d869
jdmcclain47 <jmcclain@princeton.edu>
2018-01-23 13:45:13 -0500

    Changed division by integers to division by floats

--- integrate/stochastic/monte_carlo.py
+++ integrate/stochastic/monte_carlo.py
@@ -9,13 +9,13 @@ def monte_1d(x, f, trials):
    b = x[1]
    d = (b - a) *  np.random.rand(1, trials) + a
    y = f(d)
-   return (b-a) * np.sum(y) / trials
+   return (b-a) * np.sum(y) / (1.*trials)
 
 def monte_2d(f, v, domain, trials):
    area = np.prod(domain[1] - domain[0])
    matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
-   v_eval = v(matrix) 
+   v_eval = v(matrix)
    idx = np.where(v_eval < 1.0)
    points_inside = v_eval[idx]
    k = points_inside.size
-   return area * k / trials
+   return area * k / (1.*trials)
